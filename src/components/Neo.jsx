992fb4a3e1d10ad21cbeee5d166adac66149b492
Blake Sadoway <blakesadoway@gmail.com>
2018-01-30 07:27:12 +0000

    rendered neos based on count

--- src/components/Neo.jsx
+++ src/components/Neo.jsx
@@ -5,17 +5,21 @@ class Neo extends Component{
     super();
   }
 
-  // gets orbit based on data pulled in
-  getOrbit(){
-    // change css here to new NEO orbit
+  componentDidMount(){
+    console.log(this.props.distance);
   }
+
 
   render(){
     return(
-      <div>
-        <img src='../../public/assets/images/neo.svg' className="neo" />
+      <div className="neo">
+        <img src='../../public/assets/images/neo.svg' className="neo"/>
+        <p>{this.props.distance}kms</p>
       </div>
+
     )
   }
 
 }
+
+export default Neo;
