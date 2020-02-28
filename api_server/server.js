6db3d2bab122996a86813ee513207ba8b065cd8f
Frank Lam <frank.is.missing@gmail.com>
2020-02-27 16:44:11 -0800

    Adjusted docker-compose to only bind to the /json directory

--- api_server/server.js
+++ api_server/server.js
@@ -1,13 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-// const path = require('path');
-const routes = require('./routes');
-const cors = require('cors');
-const api = require('./lib/api.js');
+const routes = require("./routes");
+const cors = require("cors");
+const api = require("./lib/api.js");
 
-// app.use(express.static("public"));
-// app.use(cors());
+app.use(cors());
 
 app.use(routes(api));
 
-app.listen(3001, () => console.log('API Server listening on port 3001'))
+app.listen(3001, () => console.log("API Server listening on port 3001"));
--- production_server.js
+++ production_server.js
@@ -4,14 +4,16 @@ const proxy = require('http-proxy-middleware');
 const compression = require('compression');
 const path = require('path');
 const PORT = 3000;
-const target = process.env.NODE_ENV === 'production' ? process.env.ASTRRISK_SERVER_DOMAIN : 'http://localhost:3001';
+const PRODUCTION_ENV = process.env.NODE_ENV === 'production'
+const target = PRODUCTION_ENV ? process.env.ASTRRISK_SERVER_DOMAIN : 'http://localhost:3001';
 
 app.use(compression());
 
 app.use(express.static(path.join(__dirname)));
 
 app.use('/api', proxy({
-  target
+  target,
+  changeOrigin: PRODUCTION_ENV
 }));
 
 app.listen(`${PORT}`, () => {
